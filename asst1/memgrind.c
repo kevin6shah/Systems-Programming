3aa413755f25eee60b8378116d2f5d680d678e0e
Kevin Shah <kevin6shah@gmail.com>
2019-03-07 16:36:26 -0500

    Let's start on memgrind now

--- asst1/memgrind.c
+++ asst1/memgrind.c
@@ -1,64 +1,6 @@
 #include "mymalloc.h"
 
 int main(int argc, char** argv) {
-	/*char* vp;
-	vp = malloc(17);
-	malloc(-12);
-	vp = malloc(17);
-	malloc(45);
-	malloc(33);
-	print();
-	void *a = malloc(9);
-    free(a);
-    free(a);
-    free(vp);
-    void *p = malloc(123);
-    printf("hey");
-    free(p);
-    printf("hey");
-     */
-    /*
-    int i;
-    while (i < 500){
-        void *ptr = malloc(1);
-        free(ptr);
-        i++;
-    }
-     */
-	//print();
-	//*(short*)(vp-2) = -17;
-	//malloc(-12);
-	//void *a = malloc(18);
-	//void *b = malloc(45);
-	//void *c = malloc(33);
-    //void *d = malloc(20);
-	//void *b = malloc(3980);
-	//void *a = malloc(9);
-    //free(a);
-    //printf("hey");
-    //free(c);
-    //printf("ho");
-    //free(b);
-    //printf("hi");
-    //free(vp);
-    //free(b);
-    //free(b);
-    
-    
-
-    
-    
-        //malloc(17);
-        //malloc(-12);
-        //malloc(17);
-        //malloc(45);
-        malloc(4088);
-        void *p = malloc(2);
-        print();
-        free(p);
-        print();
-        return 0;
-    
-    
-	
+	// Lets starts working on the memgrind now and make sure we only use
+	// git push and git pull, no more git push origin master stuff
 }
